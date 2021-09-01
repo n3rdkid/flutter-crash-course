50dc9418cc0988c1d79ca4bb1551428338c5e319
Saurav <sauravads123@gmail.com>
2021-09-01 14:52:36 +0545

    basic widgets : container & text

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,24 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("First App"),
-      ),
-      body: Container(child: Center(child: Text("Hello World"))),
-    );
+        appBar: AppBar(
+          title: Text("First App"),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                color: Colors.amber,
+                gradient: LinearGradient(colors: [Colors.amber, Colors.pink]),
+                borderRadius: BorderRadius.circular(8)),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            child: Text(
+              "BOX",
+              style: TextStyle(color: Colors.amber, fontSize: 24),
+            ),
+          ),
+        ));
   }
 }
