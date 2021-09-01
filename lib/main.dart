a674fc1a36a563c28060f69def07de84b56bd4e2
Saurav <sauravads123@gmail.com>
2021-09-01 16:15:15 +0545

    explored FAB,Drawer, Image

--- lib/main.dart
+++ lib/main.dart
@@ -3,43 +3,57 @@ import 'package:flutter/cupertino.dart';
 
 main() {
   runApp(MaterialApp(
-    title: "My first App",
-    home: Container(
-      child: HomePage(),
-    ),
-  ));
+      title: "My first App",
+      home: Container(
+        child: HomePage(),
+      ),
+      theme: ThemeData(primarySwatch: Colors.amber)));
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("First App"),
+      appBar: AppBar(
+        title: Text("First App"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            // DrawerHeader(
+            //   child: Text(
+            //     "Drawer Menu",
+            //     style: TextStyle(fontSize: 16, color: Colors.white),
+            //   ),
+            //   decoration: BoxDecoration(color: Colors.blue),
+            // ),
+            UserAccountsDrawerHeader(
+                accountName: Text("Saurav Adhikari"),
+                accountEmail: Text("sauravads123@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://images.unsplash.com/photo-1630181897844-7fb328dc7b77?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"))),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Category"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications),
+              title: Text("Notifications"),
+              subtitle: Text("Category One"),
+              trailing: Icon(Icons.edit),
+            )
+          ],
         ),
-        body: Center(
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-              ),
-              Container(
-                padding: EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-              )
-            ])));
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
