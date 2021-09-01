dc053a0ccc517225dd7e4450d66a62985b70cdfa
Saurav <sauravads123@gmail.com>
2021-09-01 15:05:57 +0545

    explored : Row, Column, Padding and Align widgets

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,28 @@ class HomePage extends StatelessWidget {
           title: Text("First App"),
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(16),
-            decoration: BoxDecoration(
-                color: Colors.amber,
-                gradient: LinearGradient(colors: [Colors.amber, Colors.pink]),
-                borderRadius: BorderRadius.circular(8)),
-            alignment: Alignment.center,
-            width: 100,
-            height: 100,
-            child: Text(
-              "BOX",
-              style: TextStyle(color: Colors.amber, fontSize: 24),
-            ),
-          ),
-        ));
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.red,
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.yellow,
+              ),
+              Container(
+                padding: EdgeInsets.all(8),
+                width: 100,
+                height: 100,
+                color: Colors.green,
+              )
+            ])));
   }
 }
