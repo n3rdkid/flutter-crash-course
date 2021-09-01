15d6607006a618f68ab3625814e2e8f9264edb23
Saurav <sauravads123@gmail.com>
2021-09-01 17:04:05 +0545

    fix : issue in test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,6 +6,7 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_crash_course/screens/homepage.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:flutter_crash_course/main.dart';
@@ -13,7 +14,7 @@ import 'package:flutter_crash_course/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(HomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
